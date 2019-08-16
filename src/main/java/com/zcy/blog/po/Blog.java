1026077c03f1456d6b84cd3b932445fea0126413
FrankZhu <zhu.cy@outlook.com>
2019-08-16 14:10:40 +0800

    Hibernate自动建数据库表

---
+++ src/main/java/com/zcy/blog/po/Blog.java
@@ -0,0 +1,46 @@
+package com.zcy.blog.po;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "t_blog")
+
+public class Blog {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String content;
+    private String firstPicture;
+    private String flag;
+    private Integer views;
+    private boolean appreciation;
+    private boolean shareStatement;
+    private boolean commentabled;
+    private boolean published;
+    private boolean recommend;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateTime;
+
+    @ManyToOne
+    private Type type;
+
+    @ManyToMany(cascade={CascadeType.PERSIST})
+    private List<Tag> tags=new ArrayList<>();
+
+    @ManyToOne
+    private User user;
+
+    public Blog(){
+
+    }
+}
