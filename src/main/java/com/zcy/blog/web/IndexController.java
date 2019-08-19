4e827474d93031c212a985033e0ca3cdf19e20bd
FrankZhu <zhu.cy@outlook.com>
2019-08-19 10:55:02 +0800

    实现全局查询功能

--- src/main/java/com/zcy/blog/web/IndexController.java
+++ src/main/java/com/zcy/blog/web/IndexController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -36,6 +37,13 @@ public class IndexController {
         return "index";
     }
 
+    @PostMapping("/search")
+    public String search(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+                         @RequestParam String query, Model model) {
+        model.addAttribute("page", blogService.listBlog("%"+query+"%", pageable));
+        model.addAttribute("query", query);
+        return "search";
+    }
     @GetMapping("/blog/{id}")
     public String blog() {
         return "blog";
